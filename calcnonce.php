8951032cafc8b5ab07962a38326690e6941e2bd3
Ulisses Flores <c.ulisses@gmail.com>
2019-08-20 22:38:38 -0300

    funcao no arquivo

--- calcnonce.php
+++ calcnonce.php
@@ -1,5 +1,5 @@
 <?php
-include 'functions.php';
+
 
 $data = $_GET['data'];
 
@@ -9,6 +9,41 @@ $nonce['NONCE'] = calcNonce($data);
 
 echo json_encode($nonce);
 
+function calcNonce($data){
+    //the difficulty will be kept below level six to ensure acceptable mining time.
+    
+     $difficulty = 2;
+    
+    
+    
+    //creates a variable with the amount of zeros d to validate a hash with
+    //the difficulty defined in the variable $ difficulty...
+    
+    $zeros = $strZero = "";
+    
+    for ($i = 1; $i <= $difficulty; $i++) {
+        $zeros = $zeros . 0;
+        $strZero = $strZero . "0";
+    }
+    
+    //compares the first few characters of the hash and checks
+    //the condition set by the difficulty.
+    
+    $nonce = 1;
+    do {
+        $try = $data . $nonce;
+        $hash = hash("sha256", $try);
+        $dific = substr($hash, 0, $difficulty);
+        $nonce++;
+        
+    } while($dific <> $zeros);
+    
+    //Return of nonce.
+    $nonceArray['NUMBER'] = $nonce -1;
+    $nonceArray['HASH'] = $hash;
+    
+    return $nonceArray;
+}
 
 
 ?>
